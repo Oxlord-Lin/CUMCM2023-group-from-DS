f82309d10681906b87f39a8d53e3415f8834bac2
qch-FA <qiuchenhao2013@163.com>
2023-09-09 23:23:32 +0800

    修改了问题3的代码，问题4左侧3个矩形的代码

--- 问题四/Q4test3.m
+++ 问题四/Q4test3.m
@@ -1,9 +1,9 @@
 options = optimoptions('fmincon','Algorithm','sqp','MaxFunctionEvaluations',15000);
 % options=optimoptions(@fmincon,'MaxFunctionEvaluations',150000);
-n=34;
-l=4;
-w=2;
+n =48;
 % x0=zeros(n,1);
+l=1.98;
+w=2.14;
 x0 = linspace(0,l,n)';
 A=[];
 b=[];
@@ -11,8 +11,12 @@ Aeq=[];
 beq=[];
 lb=zeros(n,1);
 ub=l*ones(n,1);
-nonlcon=@cons;
+nonlcon=@cons3;
 fun=@(x)(w*n+x(n)-x(1));
 x=fmincon(fun,x0,A,b,Aeq,beq,lb,ub,nonlcon,options);
+
 x
 fun(x)
+% for i=2:n
+% overlap1(x(i-1),x(i))
+% end
